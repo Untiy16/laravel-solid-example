8ee508496532a87176a3d9276f392dbefda67b35
Mykola Hlushchenko <mykola.hlushchenko@onix-systems.com>
2024-05-29 21:29:33 +0300

    customs tests

--- tests/Feature/DepartmentControllerTest.php
+++ tests/Feature/DepartmentControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\ReportType;
 use App\Models\Department;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,7 +15,7 @@ class DepartmentControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public array $departmentJsonStructure = [
+    public array $departmentJsonStructureMultiple = [
         'data' => [
             '*' => [
                 'id',
@@ -24,25 +25,32 @@ class DepartmentControllerTest extends TestCase
         ]
     ];
 
+    public array $departmentJsonStructureSingle = [
+        'data' => [
+            'id',
+            'name',
+            'address',
+        ]
+    ];
+
     public function test_get_empty_departments_list(): void
     {
         $response = $this->getJson('/api/departments');
-
         $response
             ->assertStatus(200)
-            ->assertJsonStructure($this->departmentJsonStructure)
+            ->assertJsonStructure($this->departmentJsonStructureMultiple)
             ->assertJsonCount(0, 'data');
     }
 
     public function test_get_not_empty_departments_list(): void
     {
-        Department::factory()->count(1)->create();
-        $response = $this->getJson('/api/departments');
+        Department::factory()->count(5)->create();
 
+        $response = $this->getJson('/api/departments');
         $response
             ->assertStatus(200)
-            ->assertJsonStructure($this->departmentJsonStructure)
-            ->assertJsonCount(1, 'data');
+            ->assertJsonStructure($this->departmentJsonStructureMultiple)
+            ->assertJsonCount(5, 'data');
     }
 
     public function test_get_non_existent_department_by_id()
@@ -51,38 +59,18 @@ class DepartmentControllerTest extends TestCase
 
         $response = $this->getJson($url);
         $response->assertNotFound();
-
-        $this->assertThrows(
-            fn () => $this->withoutExceptionHandling()->getJson($url),
-            ModelNotFoundException::class
-        );
     }
 
     public function test_get_existent_department_by_id()
     {
-        Department::factory()->count(1)->create();
+        $department = Department::factory()->create();
 
-        $response = $this->getJson('/api/departments/1');
-
+        $response = $this->getJson("/api/departments/$department->id");
         $response
             ->assertStatus(200)
-            ->assertJsonStructure([
-                'data' => [
-                    'id',
-                    'name',
-                    'address',
-                ]
-            ]);
+            ->assertJsonStructure($this->departmentJsonStructureSingle);
     }
 
-    #[DataProvider('invalidDepartmentData')]
-    public function test_create_department_with_invalid_data($data, $fields): void
-    {
-       $response = $this->postJson('/api/departments', $data);
-       $response
-           ->assertStatus(422)
-           ->assertJsonValidationErrors($fields);
-    }
 
     public static function invalidDepartmentData(): array
     {
@@ -97,5 +85,102 @@ class DepartmentControllerTest extends TestCase
             [['name' => 'n', 'address' => 'Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605'], ['name']],
             [['name' => 'validName', 'address' => 'Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605'], ['address']],
         ];
+    }
+
+    public static function validDepartmentData(): array
+    {
+        return [
+            [['name' => 'validName', 'address' => 'Suite 246 81515 Osinski Manor, East Luke, TX 59690-4605']],
+        ];
+    }
+    #[DataProvider('invalidDepartmentData')]
+    public function test_create_department_with_invalid_data($data, $fields): void
+    {
+       $response = $this->postJson('/api/departments', $data);
+       $response
+           ->assertStatus(422)
+           ->assertJsonValidationErrors($fields);
+    }
+    #[DataProvider('validDepartmentData')]
+    public function test_create_department_with_valid_data($data): void
+    {
+       $response = $this->postJson('/api/departments', $data);
+
+       $response
+           ->assertCreated()
+           ->assertJsonStructure($this->departmentJsonStructureSingle);
+    }
+    #[DataProvider('validDepartmentData')]
+    public function test_update_non_existent_department($data)
+    {
+        $url = '/api/departments/1';
+
+        $response = $this->putJson($url, $data);
+
+        $response->assertNotFound();
+    }
+
+    #[DataProvider('invalidDepartmentData')]
+    public function test_update_existent_department_with_invalid_data($data, $fields)
+    {
+        $department = Department::factory()->create();
+        $url = "/api/departments/$department->id";
+
+        $response = $this->putJson($url, $data);
+        $response
+           ->assertStatus(422)
+           ->assertJsonValidationErrors($fields);
+    }
+
+    public function test_delete_non_existent_department()
+    {
+        $response = $this->deleteJson('/api/departments/1');
+        $response->assertNotFound();
+//        $this->assertThrows(
+//            fn () => $this->withoutExceptionHandling()->deleteJson($url),
+//            ModelNotFoundException::class
+//        );
+    }
+
+    public function test_delete_existent_department()
+    {
+        $department = Department::factory()->create();
+        $url = "/api/departments/$department->id";
+
+        $deleteResponse = $this->deleteJson($url);
+        $deleteResponse
+            ->assertNoContent();
+
+        $getResponse = $this->getJson($url);
+        $getResponse->assertNotFound();
+    }
+    public function test_departments_report_with_wrong_report_type()
+    {
+        $this->postJson('/api/departments/report/1/invalidType')
+            ->assertBadRequest()
+            ->assertJson(['message' => 'Report Type not found']);
+    }
+
+    #[DataProvider('reportTypes')]
+    public function test_departments_report_with_valid_report_types($data)
+    {
+        $department = Department::factory()->create();
+
+        $response = $this->postJson("/api/departments/report/$department->id/$data");
+        $response
+            ->assertCreated()
+            ->assertJsonStructure([
+                'department' => [
+                    'id',
+                    'name',
+                ],
+                'rows',
+                'summary'
+            ]);
+    }
+
+    public static function reportTypes(): array
+    {
+        return array_map(fn($i) => [$i], array_column(ReportType::cases(), 'value'));
     }
 }
